181ce8f2d7c14ce21d3e9ecec6641d35bb8c54e8
maxbbn <qipbbn@gmail.com>
2012-04-01 11:09:32 +0800

    配置更新，颜色引擎更新

--- src/chart/chart/data.js
+++ src/chart/chart/data.js
@@ -36,8 +36,9 @@ KISSY.add(function(S, Util){
         //Number Format
         //针对特定数据设置numberFormat可以用
         //numberFormats : ['0.00','0.000']
-        //false表示不做format处理
-        numberFormat : false,
+        //null 表示不做format处理
+        //默认为 null
+        numberFormat : null,
         //边框颜色
         frameColor : "#d7d7d7"
     };
@@ -61,26 +62,16 @@ KISSY.add(function(S, Util){
             paddingBottom : 10,
             paddingLeft : 10,
             paddingRight : 10,
-            shadow : true,
-            labelTemplete : "{name} {pecent}%", //{name} {data} {pecent}
+            drawShadow : true,
+            shadow : {
+                blur : 3,
+                color : '#000'
+            },
+            labelTemplate : "{name} {pecent}%", //{name} {data} {pecent}
             firstPieOut : false // 第一块飞出
         }
     };
 
-    /**
-     * 数据默认配置
-     */
-    var defaultElementConfig = {
-        'default' : {
-            label : "{name} -  {data}"
-        },
-        'line':{
-        },
-        "pie" : {
-        },
-        "bar" : {
-        }
-    };
 
     /**
      * 图表数据
@@ -88,9 +79,9 @@ KISSY.add(function(S, Util){
      * @constructor
      * @param {Object} 输入的图表JSON数据
      */
-    function Data(data){
-        if(!data || !data.type) return;
-        if(!this instanceof Data)
+    function Data(data) {
+        if (!data || !data.type) return;
+        if (!this instanceof Data)
             return new Data(data);
         var self = this,
             cfg = data.config;
@@ -101,7 +92,6 @@ KISSY.add(function(S, Util){
         //self._design = data.design;
 
         self.config = cfg = S.merge(defaultConfig, specificConfig[self.type], cfg);
-
         /**
          * 配置兼容
          */
@@ -110,14 +100,13 @@ KISSY.add(function(S, Util){
             'top'   : 'paddingTop',
             'bottom': 'paddingBottom',
             'right' : 'paddingRight'
-        }, function(item, key){
-            if(key in cfg && S.isNumber(cfg[key])){
+        }, function (item, key) {
+            if (key in cfg && S.isNumber(cfg[key])) {
                 cfg[item] = cfg[key];
             }
         });
 
-        self._elements = self._initElement(data);
-        self._elements = self._expandElement(self._elements);
+        self._elements = self._expandElement(self._initElement(data));
         self._initElementItem();
         self._axis = data.axis;
     }
@@ -126,24 +115,24 @@ KISSY.add(function(S, Util){
         /**
          * get the AxisData
          */
-        axis : function(){
+        axis : function () {
             return this._axis;
         },
 
         /**
          * get the Element Data
          */
-        elements : function(){
+        elements : function () {
             return this._elements;
         },
 
         /**
          * get the the max length of each Element
          */
-        maxElementLength: function(){
+        maxElementLength: function () {
             var ml = 0;
-            S.each(this._elements, function(elem,idx){
-                if(S.isArray(elem.items)){
+            S.each(this._elements, function (elem,idx) {
+                if (S.isArray(elem.items)) {
                     ml = Math.max(ml, elem.items.length);
                 } else {
                     ml = Math.max(ml,1)
@@ -160,15 +149,15 @@ KISSY.add(function(S, Util){
          * @param {Number} the index of the element
          * @param {String} type of Chart
          */
-        getColor : function(idx,type){
+        getColor : function (idx,type) {
             var length = this._elements.length;
             var usercolor = this.config.colors
-            if(S.isArray(usercolor) && usercolor[idx]){
+            if (S.isArray(usercolor) && usercolor[idx]) {
                 return usercolor[idx];
             }
 
             //getColor frome user defined function
-            if(S.isFunction(usercolor)){
+            if (S.isFunction (usercolor)) {
                 return usercolor(idx);
             }
 
@@ -179,9 +168,9 @@ KISSY.add(function(S, Util){
         /**
          * return the sum of all Data
          */
-        sum: function(){
+        sum: function () {
             var d = 0;
-            this.eachElement(function(item){
+            this.eachElement(function (item) {
                 d += item.data;
             });
             return d;
@@ -190,7 +179,7 @@ KISSY.add(function(S, Util){
         /**
          * Get the Biggest Data from element
          */
-        max : function(){
+        max : function () {
             return this._max;
         },
 
@@ -199,33 +188,33 @@ KISSY.add(function(S, Util){
          * @param {Number} index of element
          * @param {Number} length of element
          */
-        getDefaultColor : function (idx,length){
+        getDefaultColor : function (idx, length) {
             //在色相环上取色
-            var colorgap = 1/3,
+            var colorgap = 1/5,
+                colorStart = 15 /360,
+                colorScope = .50,
                 //h = Math.floor(idx/3)/length + 1/(idx%3 + 1)*colorgap,
-                h = colorgap * idx, //h of color hsl
-                s = .7, // s of color hsl
-                b = 1,//b of  color hsb
-                l = b - s*.5, //l of color hsl
+                h = 0, //h of color hsl
+                s = .88, // s of color hsl
+                b = .85,//b of  color hsb
+                l = b - s * .5, //l of color hsl
                 i, j, k;
+            h = colorStart + idx * colorScope / (length - 1)
+            // if (idx < 3) {
+            //     h = colorgap * idx + 0.01;
+            // } else {
+            //     //防止最后一个颜色落在第3区间
+            //     if (length % 3 == 0) {
+            //         if (idx === length -1) {
+            //             idx = length - 2;
+            //         } else if (idx === length - 2) {
+            //             idx = length - 1;
+            //         }
+            //     }
+            //     k = Math.ceil((idx + 1)/3);
 
-            if(idx < 3){
-                h = colorgap * idx + 0.05;
-            }else{
-                //防止最后一个颜色落在第3区间
-                if(length % 3 == 0){
-                    if(idx === length -1){
-                        idx = length - 2;
-                    }else
-                    if(idx === length - 2){
-                        idx = length - 1;
-                    }
-                }
-                i = idx % 3;
-                j = Math.ceil(length/3);
-                k = Math.ceil((idx + 1)/3);
-                h = i*colorgap + colorgap/j * (k-1);
-            }
+            //     h = (idx % 3)*colorgap + colorgap/(Math.ceil(length/3)) * (k-1);
+            // }
             return Util.Color.hsl(h,s,l).hexTriplet();
         },
 
@@ -233,81 +222,77 @@ KISSY.add(function(S, Util){
         /**
          * execuse fn on each Element item
          */
-        eachElement : function(fn){
+        eachElement : function (fn) {
             var self = this;
 
-            S.each(self._elements, function(item,idx){
-                if(item.items){
-                    S.each(item.items, function(i, idx2){
+            S.each(self._elements, function (item,idx) {
+                if (item.items) {
+                    S.each(item.items, function (i, idx2) {
                         fn(i,idx,idx2);
                     });
-                }else{
+                } else {
                     fn(item, idx, -1);
                 }
             });
         },
 
+
         /**
-         * Init the Element Item
-         * parse the label
+         * normalize Input Element
+         * @private
+         * @param {Object} input data
          */
-        _initElementItem: function(){
-            var self = this;
-            self._max = null;
+        _initElement : function (data) {
+            var elements = null,
+                elem,
+                self = this,
+                newelem;
 
-            self.eachElement(function(elem,idx,idx2){
-                if(idx === 0 && (!idx2) )self._max = elem.data || 0;
+            //数组形式
+            if (data.elements && S.isArray(data.elements)) {
+                elements = S.clone(data.elements);
+            }
 
-                var defaultElem = S.merge(defaultElementConfig['default'], defaultElementConfig[self.type]||{});
+            //对象形式
+            if (!data.elements && data.element && S.isArray(data.element.names)) {
+                elements = [];
+                elem = data.element;
+                S.each(elem.names, function　(name,　idx) {
+                    elements.push({
+                        name   : name,
+                        data   : S.isArray(elem.datas) ? elem.datas[idx] : null,
+                        label  : S.isArray(elem.labels) ? elem.labels[idx] : elem.label
+                    });
+                });
+            }
 
-                elem.data = S.isNumber(elem.data) ? elem.data : 0;
-                //数字的格式
-                if(S.isArray(self.config.numberFormats) ){
-                    elem.format = self.config.numberFormats[idx];
-                }
-
-                if(typeof elem.format == 'undefined' ){
-                    elem.format = self.config.numberFormat;
-                }
-
-                elem.label = elem.label || defaultElem.label;
-                elem.label = S.substitute(
-                    elem.label,
-                    {
-                        name : elem.name,
-                        data : elem.format ? Util.Format(elem.data, elem.format) : elem.data
-                    }
-                );
-                self._max = Math.max(self._max, elem.data);
-            });
+            return elements;
         },
-
         /**
          * expand the sub element
          * @param {Object} the Element Object
          * @private
          */
-        _expandElement : function(data){
+        _expandElement : function (data) {
             var datas,
-                itemdata,
                 self = this;
-            S.each(data, function(item,idx){
-                if(S.isArray(item.datas)){
-                    item.items = item.items || [];
-                    S.each(item.datas, function(d,n){
-                        itemdata = {
-                            name : item.name,
+            S.each(data, function (element, idx) {
+
+                if (S.isArray(element.datas)) {
+
+                    element.items = [];
+
+                    S.each(element.datas, function (d, index) {
+                        var itemdata = {
+                            name : element.name,
                             data : d
                         };
-                        if(item.label){
-                            itemdata.label = item.label;
-                        }
-                        if(item.labels && S.isString(item.labels[n])){
-                            itemdata.label = item.labels[n];
-                        }
 
-                        delete item.datas;
-                        item.items.push(itemdata);
+
+                        itemdata.label = S.isArray(element.labels) ? 
+                            element.labels[index] : element.label;
+
+                        element.items.push(itemdata);
                     });
                 }
 
@@ -315,55 +300,50 @@ KISSY.add(function(S, Util){
             return data;
         },
 
-        /**
-         * normalize Input Element
-         * @private
-         * @param {Object} input data
-         */
-        _initElement : function(data){
-            var elements = null,
-                elem,
-                self = this,
-                newelem;
-
-            //数组形式
-            if(data.elements && S.isArray(data.elements)){
-                elements = S.clone(data.elements);
-            }
-
-            //对象形式
-            if(!data.elements && data.element && S.isArray(data.element.names)){
-                elements = [];
-                elem = data.element;
-                S.each(elem.names, function(name,idx){
-                    elements.push({
-                        name   : name,
-                        data   : self._getLabel(elem.datas, idx),
-                        label  : self._getLabel(elem.labels, idx) || elem.label
-                    });
-                });
-            }
-
-            return elements;
-        },
 
         /**
-         * 如果是数组，返回label[n]
-         * 否则返回labels
-         * @private
-         * @param {Any} labels of chart
-         * @param {Number} offset of label
+         * Init the Element Item
+         * parse the label
          */
-        _getLabel : function(labels, offset){
-            if(S.isArray(labels)){
-                return (offset < labels.length) ? labels[offset]:null;
-            }else{
-                return false;
-            }
+        _initElementItem: function () {
+            var self = this;
+
+            self._max = 0;
+
+            self.eachElement(function (elem,idx,idx2) {
+                var label;
+
+                //统计最大值
+                if (idx === 0 && (!idx2)) self._max = elem.data || 0;
+                elem.data = S.isNumber(elem.data) ? elem.data : 0 ;
+                self._max = Math.max(self._max, elem.data);
+
+                
+
+                //数字的格式
+                if (S.isArray(self.config.numberFormat) ) {
+                    elem.format = self.config.numberFormat[idx];
+                } else if (S.isString(self.config.numberFormat)) {
+                    elem.format = self.config.numberFormat;
+                }
+
+                if (typeof elem.format == 'undefined' ) {
+                    elem.format = self.config.numberFormat;
+                }
+
+                label = (typeof elem.label === 'undefined') ? null : elem.label;
+
+                elem.label = S.substitute(
+                    label, {
+                        name : elem.name,
+                        data : elem.format ? Util.numberFormat(elem.data, elem.format) : elem.data
+                    }
+                );
+
+            });
         }
     });
 
-    P.Data = Data;
     return Data;
 
 }, {
